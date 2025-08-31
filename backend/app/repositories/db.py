f81e1021665b9331e4b5bac458b77220cd456d4c
Thomas Cionek <thomascionek97@gmail.com>
2025-08-31 11:23:02 -0300

    Add db init and get_connection helper

---
+++ backend/app/repositories/db.py
@@ -0,0 +1,18 @@
+import psycopg2
+from psycopg2.pool import SimpleConnectionPool
+from typing import Optional
+
+
+_pool: Optional[SimpleConnectionPool] = None
+
+
+def init_pool(dsn: str):
+    global _pool
+    if _pool is None:
+        _pool = SimpleConnectionPool(minconn=1, maxconn=10, dsn=dsn)
+    return _pool
+
+
+def get_conn():
+    if _pool is None:
+        raise RuntimeError("DB pool not initialized")
