3e6f26839d60ac677bc043ce718020923c483c7c
Thomas Cionek <thomascionek97@gmail.com>
2025-08-31 11:03:57 -0300

    Add psycopg2 database connection handler

---
+++ repositories/db.py
@@ -0,0 +1,23 @@
+import psycopg2
+from psycopg2.pool import SimpleConnectionPool
+from typing import Optional
+
+_pool: Optional[SimpleConnectionPool] = None
+
+
+def init_pool(dsn: str):
+    global _pool
+    if _pool is None:
+        _pool = SimpleConnectionPool(minconn=1, maxconn=10, dsn=dsn)
+    return _pool
+
+
+def get_conn():
+    if _pool is None:
+        raise RuntimeError("DB pool not initialized")
+    return _pool.getconn()
+
+
+def put_conn(conn):
+    if _pool is not None and conn is not None:
+        _pool.putconn(conn)
