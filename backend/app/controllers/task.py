ee5bcf30e6fb4a1e437a324222572a2df641bca5
Thomas Cionek <thomascionek97@gmail.com>
2025-10-01 14:39:34 -0300

    Create task endpoint

---
+++ backend/app/controllers/task.py
@@ -0,0 +1,18 @@
+from flask import request, jsonify
+from . import api
+from app.models.task import TaskCreate
+from app.services.task import Tasks
+
+
+@api.post("/tasks")
+def create_task():
+    data = request.get_json(force=True, silent=False)
+    payload = TaskCreate(**data)
+    task = Tasks.create(payload)
+    return jsonify(task.model_dump()), 201
+
+
+@api.get("/tasks")
+def list_tasks():
+    tasks = Tasks.get_many()
+    return jsonify([t.model_dump() for t in tasks]), 200
