5930ad695d4d88470b6ea78665c584a5b9956afd
Thomas Cionek <thomascionek97@gmail.com>
2025-10-01 11:47:36 -0300

    Add comentary on default created_at

--- backend/app/models/task.py
+++ backend/app/models/task.py
@@ -6,9 +6,9 @@ from pydantic import BaseModel, Field
 class TaskCreate(BaseModel):
     name: str = Field(min_length=1, max_length=200)
     value: Optional[float] = None
-    # created_at: datetime # this is auto set by the database
     scheduled_to: datetime
     executed_at: Optional[datetime] = None
+    # created_at: datetime # this is auto set by the database
 
 
 class TaskRead(BaseModel):
